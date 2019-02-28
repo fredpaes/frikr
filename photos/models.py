1c79737dd9abc97b013480f80110cb7666e49797
Fred <fred.paucarespinoza@gmail.com>
2019-02-27 20:36:29 -0500

    add model photos to migrate

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Photo(models.Model):
+    LICENSES = (
+        ('RIG', 'COPYRIGHT'),
+        ('LEF', 'COPYLEFT'),
+        ('CC', 'CREATIVE COMMONS')
+    )
+    name = models.CharField(max_length=150)
+    url = models.URLField()
+    description = models.TextField(blank=True, null=True, default='')
+    license = models.CharField(max_length=3, choices=LICENSES)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
--- photos/views.py
+++ photos/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import *
 
 # Create your views here.
+def home(request):
+    photos = Photo.objects.all()
+    return HttpResponse('Hola mundo')
+
+def test(request):
+    return HttpResponse('es una prueba')
