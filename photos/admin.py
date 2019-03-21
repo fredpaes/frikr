9f2b6bc7f52789526dbda9d7670d8a419a3b209c
Fred <fred.paucarespinoza@gmail.com>
2019-03-20 20:40:32 -0500

    admin tunning

--- photos/admin.py
+++ photos/admin.py
@@ -2,4 +2,32 @@ from django.contrib import admin
 from .models import Photo
 
 # Register your models here.
-admin.site.register(Photo)
+
+class PhotoAdmin(admin.ModelAdmin):
+    list_display = ('name', 'owner_name', 'license', 'visibility')
+    list_filter = ('license', 'visibility')
+    search_fields = ('name', 'description')
+
+    def owner_name(self, obj):
+        return '{} {}'.format(obj.owner.first_name, obj.owner.last_name)
+
+    owner_name.short_description = 'Photo owner'
+    owner_name.admin_order_field = 'owner'
+
+    fieldsets = (
+        (None, {
+            'fields': ('name', ),
+            'classes': ('wide', )
+        }),
+        ('Description & Author', {
+            'fields': ('description', 'owner'),
+            'classes': ('wide', )
+        }),
+        ('Extra', {
+            'fields': ('url', 'license', 'visibility'),
+            'classes': ('wide', 'collapse')
+        })
+    )
+
+
+admin.site.register(Photo, PhotoAdmin)
